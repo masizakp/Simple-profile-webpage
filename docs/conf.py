3fd3f4aa36d2f93fa93ea0ef9954ba54c69dff6a
Masiza Khathatso Paul <masizakp@gmail.com>
2025-07-11 23:15:56 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,10 +1,7 @@
 import os
 import sys
-import django
 
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Your_project_name.settings'
-django.setup()
 # -- Path setup --------------------------------------------------------------
 # Configuration file for the Sphinx documentation builder.
 #
@@ -27,6 +24,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
+    
 ]
 
 templates_path = ['_templates']
